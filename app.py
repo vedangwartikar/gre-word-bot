b7f312813be1caf6ab71848a2b9f2073eb807450
Vedang Wartikar <vedangwartikar17@gmail.com>
2021-02-16 11:11:03 +0530

    vet verge added

--- app.py
+++ app.py
@@ -16,10 +16,35 @@ def doge(update: Update, context: CallbackContext) -> None:
     print(doge_price_inr)
     update.message.reply_text(f'DOGE: {doge_price_inr} INR')
 
+def vet(update: Update, context: CallbackContext) -> None:
+    yahoo_financials = YahooFinancials('VET-USD')
+    vet_price = yahoo_financials.get_current_price()
 
-updater = Updater('1598163939:AAHf-__oR8j20yTBnjPkI_DNvJGd3Ww996M')
+    c = CurrencyRates()
+    inr_rate = c.get_rate('USD', 'INR')
+
+    vet_price_inr = round(vet_price * inr_rate, 4)
+    print(vet_price_inr)
+    update.message.reply_text(f'VET: {vet_price_inr} INR')
+
+updater = Updater('1657397986:AAEzFf5thjYPao8rFLXFGbB_FRW6K19FgBo')
+
+def verge(update: Update, context: CallbackContext) -> None:
+    yahoo_financials = YahooFinancials('XVG-USD')
+    verge_price = yahoo_financials.get_current_price()
+
+    c = CurrencyRates()
+    inr_rate = c.get_rate('USD', 'INR')
+
+    verge_price_inr = round(verge_price * inr_rate, 4)
+    print(verge_price_inr)
+    update.message.reply_text(f'VERGE: {verge_price_inr} INR')
+
+updater = Updater('1657397986:AAEzFf5thjYPao8rFLXFGbB_FRW6K19FgBo')
 
 updater.dispatcher.add_handler(CommandHandler('doge', doge))
+updater.dispatcher.add_handler(CommandHandler('vet', vet))
+updater.dispatcher.add_handler(CommandHandler('verge', verge))
 
 updater.start_polling()
 updater.idle()
